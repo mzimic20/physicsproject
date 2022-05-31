785a14009b5f272aab263bae083b9eed04a58dcc
Cullen <cullenyanderson@gmail.com>
2022-05-31 13:25:23 -0400

    Bare bones questions

--- main.js
+++ main.js
@@ -32,7 +32,40 @@ velocitySlider.oninput = function() {
 }
 
 function test() {
-    alert(electricFieldSlider.value);
+    alert(velocitySlider.value);
+}
+
+// Bare Bones Outline
+// Generate random ef, mf and corresponding correct velocity
+// Display question on website containing these variables.
+// Compare user input (via sliders) to correct answer
+// Alert if they are correct or wrong
+// After, display nicer way to show if they are correct
+// Add button to go on to new question
+// Add different kinds of questions
+// Incorporate animation
+// Fix sliders to values given in question. Also add nicer way to input and view variables.
+
+// Maybe make a number generator to only generate numbers with integer answers
+var ef = Math.floor(Math.random() * 100 + 1);
+var mf = Math.floor(Math.random() * 10 + 1);
+var answer = (ef / mf).toFixed(1);
+var question = `Electric Field is ${ef} N/C. Magnetic Field is ${mf} T. The particle is to be shot in a straight line. What initial velocity should it have? Answer is ${answer} m/s`;
+document.getElementById('question').innerHTML = question
+
+
+function checkApprox(num1, num2, epsilon) {
+    if (Math.abs(num1 - num2) <= epsilon) {
+        return true
+    }
+}
+
+function submit() {
+    if (checkApprox(velocitySlider.value, answer, 2)) {
+        alert('Correct! Refresh the page to try again!');
+    } else {
+        alert('Incorrect! ' + 'Your answer: ' + velocitySlider.value + ' Correct Answer: ' + answer);
+    }
 }
 
 
