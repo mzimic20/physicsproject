b8c991db78f30a5c62edd079db4ca4f982632d7b
mzimic20 <mzimic20@stuy.edu>
2022-06-01 21:33:02 -0400

    capacitor image, revised animation function, reset button

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-let E = document.getElementById("electric_field_slider")
-let B = document.getElementById("magnetic_field_slider")
-let q = document.getElementById("charge_slider")
-let v = document.getElementById("velocity_slider")
-
 function run() {
+	var E = document.getElementById("electric_field_slider").value
+	var B = document.getElementById("magnetic_field_slider").value
+	var q = document.getElementById("charge_slider").value
+	var v = document.getElementById("velocity_slider").value
+
 	let id = null;
 	const elem = document.getElementById("animation");
-	let pos = 0;
+	let posx = 25;
+	let posy = 300;
+	let t = 0;
 	clearInterval(id);
 	id = setInterval(frame, 5);
 	function frame() {
-		if (pos == 350) {
+		if (posx >= 1100 || posy <= 100 || posy >= 500) {
 			clearInterval(id);
 		}
 		else {
-			pos++;
-			elem.style.top = pos + "px";
-			elem.style.left = pos + "px";
+			t++;
+			posx += v / 5;
+//			posy++;
+			elem.style.top = posy + "px";
+			elem.style.left = posx + "px";
 		}
 	}
+}
+
+function reset() {
+	let elem = document.getElementById("animation");
+	elem.style.top = "300px";
+	elem.style.left = "25px";
 }
