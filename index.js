97c3bc0d72cd59abe302a29a969d30b7727bca7f
mzimic20 <mzimic20@stuy.edu>
2022-06-02 08:34:53 -0400

    animation finished, still could use some fine tweaking but all controls are responsive

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 function run() {
-	var E = document.getElementById("electric_field_slider").value
-	var B = document.getElementById("magnetic_field_slider").value
-	var q = document.getElementById("charge_slider").value
-	var v = document.getElementById("velocity_slider").value
 
+//	fetch slider values
+	let E = document.getElementById("electric_field_slider").value;
+	let B = document.getElementById("magnetic_field_slider").value;
+	let q = document.getElementById("charge_slider").value;
+	let v = document.getElementById("velocity_slider").value;
+
+//	calculate forces
+	let Fe = E * q;
+	let Fb = q * v * B;
+
+//	prep animation
 	let id = null;
 	const elem = document.getElementById("animation");
 	let posx = 25;
 	let posy = 300;
 	let t = 0;
+
+//	animate
 	clearInterval(id);
 	id = setInterval(frame, 5);
 	function frame() {
+//		stop animation
 		if (posx >= 1100 || posy <= 100 || posy >= 500) {
 			clearInterval(id);
 		}
 		else {
+//			move
 			t++;
 			posx += v / 5;
-//			posy++;
+			posy += (Fe + Fb) * t / 7500;
 			elem.style.top = posy + "px";
 			elem.style.left = posx + "px";
 		}
